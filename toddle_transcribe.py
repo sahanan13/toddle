a9e9d4e12853af9a24ae9ea944eaf7fefc3e8e25
Minjung Kwon <Minjk121@gmail.com>
2021-11-04 22:42:49 -0400

    Has an error using upload_file function

--- toddle_transcribe.py
+++ toddle_transcribe.py
@@ -8,13 +8,13 @@ import json
 
 transcribe = boto3.client('transcribe')
 
-def get_s3uri(file_name, file_type):
+def get_s3uri(file_name, file_type, user_input):
     bucket_name = "toddle_transcribe" # let files uploaded to the same s3 bucket
-    key = "upload-file"
 
     s3 = boto3.resource('s3')
     bucket = s3.Bucket(bucket_name)
-    bucket.upload_file(file_name, key)
+    bucket.upload_file(user_input, bucket_name, file_name, ExtraArgs=None) # TODO debug usage of upload_file
+
     location = boto3.client('s3').get_bucket_location(Bucket=bucket_name)['LocationConstraint']
 
     # url = "https://s3-%s.amazonaws.com/%s/%s" % (location, bucket_name, key)
@@ -53,13 +53,21 @@ def main():
     file_type = input("type file type: ") # mp4
     user_input = input("type file path: ") # file path: C:\Users\Minjk\Downloads\toddle_lec.mp4
 
+    # print(type(job_name))
+    # print(type(file_name))
+    # print(type(file_type))
+    # print(type(user_input))
+
     assert os.path.exists(user_input), "File not found at " + str(user_input)
     print("File " + str(user_input) + "is found")
     job_file = open(user_input, 'r+')
 
-    job_uri = get_s3uri(file_name, file_type)
+    job_uri = get_s3uri(file_name, file_type, user_input)
+    print( "job uri: " + job_uri )
 
     transcribe_file(job_name, job_uri, transcribe, file_type)
+
+    # job_file.close()
 
     # job_name = 'test_mjk3'
     # job_uri = 's3://toddle-test-mjk/COLHIST12014-V008200_DTH.mp4'
