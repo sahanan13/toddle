a5e6336fdbf29869fd31f6941edd389f898d42df
Minjung Kwon <Minjk121@gmail.com>
2021-11-06 13:55:21 -0400

    fixed output to a string type text

--- toddle_transcribe.py
+++ toddle_transcribe.py
@@ -13,9 +13,9 @@ def get_s3uri(file_name, file_type, user_input):
 
     s3 = boto3.resource('s3')
     bucket = s3.Bucket(bucket_name)
-    print(user_input) 
-    print(bucket_name)
-    print(file_name)
+    # print(user_input) 
+    # print(bucket_name)
+    # print(file_name)
 
     try:
         bucket.upload_file(user_input, file_name, ExtraArgs = {}) # TODO debug usage of upload_file
@@ -28,11 +28,13 @@ def get_s3uri(file_name, file_type, user_input):
     location = boto3.client('s3').get_bucket_location(Bucket=bucket_name)['LocationConstraint']
 
     uri = "s3://%s/%s" % (bucket_name, file_name)
-    print("uri:  " + uri)
+    # print("uri:  " + uri)
     return uri
 
 
 def transcribe_file(job_name, job_uri, transcribe, file_type):
+    text_output = ""
+
     transcribe.start_transcription_job(
         TranscriptionJobName=job_name,
         Media={'MediaFileUri': job_uri},
@@ -48,21 +50,24 @@ def transcribe_file(job_name, job_uri, transcribe, file_type):
             text = data['results']['transcripts'][0]['transcript']
             print("========== below is output of speech-to-text ========================")
             print(text)
+            text_output += text
             print("=====================================================================")
             break
         print("In Progress...")
         time.sleep(10)
-    return status
+    return text_output
 
 def main():
     
     job_name = input("type job name: ") # transcribe job name
     file_name = input("type file name: ") # toddle_lec.mp4
     file_type = input("type file type: ") # mp4
-    user_input = input("type file path: ") # file path: C:\Users\Minjk\Downloads\toddle_lec.mp4
+    user_input = input("type file path: ") 
+    # file path: C:\Users\Minjk\Downloads\toddle_lec.mp4 (for windows)
+    # file path: /Users/Minjk/Downloads/toddle_lec.mp4 (for mac)
 
     assert os.path.exists(user_input), "File not found at " + str(user_input)
-    print("File " + str(user_input) + "is found")
+    print("File " + str(user_input) + " is found")
     job_file = open(user_input, 'r+')
 
     job_uri = get_s3uri(file_name, file_type, user_input)
@@ -70,6 +75,8 @@ def main():
 
     text_output = transcribe_file(job_name, job_uri, transcribe, file_type)
     # print(type(text_output))
+    # print(text_output)
+   
     
 if __name__ == '__main__':
     main()
